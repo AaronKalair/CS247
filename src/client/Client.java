f5b21886164a60d3a77714d12f7a3b9011db6ccf
Alex Baines <A.Baines@warwick.ac.uk>
2012-03-15 19:30:56 +0000

    Directory structure + ant build.xml

---
+++ src/client/Client.java
@@ -0,0 +1,18 @@
+package CS247;
+
+public class Client {
+
+	public static void main(String[] args){
+		Client client = new Client(args);
+		while(client.run());
+	}
+
+	public Client(String[] args){
+	
+	}
+
+	public boolean run(){
+		return true;
+	}
+
+}
