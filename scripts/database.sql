fa483782515e2a0902fc115396f4f9b0f255a7b6
Aaron Kalair <aaronkalair@gmail.com>
2012-04-28 20:33:48 +0100

    Created the validIP and insertIP methods. Improved ip_whitelist table design.

--- scripts/database.sql
+++ scripts/database.sql
@@ -5,7 +5,6 @@
 --
 
 CREATE TABLE ip_whitelist (
-  id int(11) NOT NULL AUTO_INCREMENT,
   ip_address varchar(16) NOT NULL COMMENT 'IP address of a trusted client',
-  PRIMARY KEY (id)
+  PRIMARY KEY (ip_address)
 ) 
--- src/server/Database.java
+++ src/server/Database.java
@@ -3,11 +3,13 @@ package CS247;
 import java.sql.*;
 
 public class Database {
+
+    private Connection conn;
     
     // Connect to the database TODO: connection pooling
-    public Database() {
+    Database() {
     
-        Connection conn = null;
+        this.conn = null;
         // Default port is 3306
         String url = "jdbc:mysql://localhost:3306/";
         // Make sure you have the CS247 database created or this wont work
@@ -23,6 +25,7 @@ public class Database {
         try {
             Class.forName(driver).newInstance();
             conn = DriverManager.getConnection(url+dbName,userName,password);
+            
         } 
           
         catch (Exception e) {
@@ -30,8 +33,7 @@ public class Database {
         }
         
         closeConnection(conn);
-        
-        
+   
     }
     
     // Not sure we need this, as we want to reuse connections ?
@@ -43,6 +45,63 @@ public class Database {
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    // Inserts a IP into the ip_whitelist table
+    public void insertIP(String ip) {
+            
+        PreparedStatement addIP = null;
+        
+        String insertIP = "INSERT INTO ip_whitelist (ip_address) values(?)";
+        
+        try {
+            addIP = conn.prepareStatement(insertIP);
+            addIP.setString(1, ip);
+            addIP.executeUpdate();
+        }
+        
+        catch (Exception e) {
+           e.printStackTrace();
+        }
+    
+    }
+    
+    // Checks if a given IP is in the ip_whitelist table
+    public boolean validIP(String ip) {
+    
+        PreparedStatement checkIP = null;
+        ResultSet results = null;
+        int numberOfResults = -100;
+        
+        String IpToCheck = "SELECT ip_address FROM ip_whitelist WHERE ip_address = ?";
+        
+        try {
+        
+            checkIP = conn.prepareStatement(IpToCheck);
+            checkIP.setString(1, ip);
+            results = checkIP.executeQuery();
+            
+            // JavaDB has no way to find out the number of rows returned without itterating through them (yes really)
+            while ( results.next() ) {
+                String id = results.getString(1);
+            }
+            
+            results.last();
+            numberOfResults = results.getRow();
+        }
+        
+        catch (Exception e) {
+           e.printStackTrace();
+        }
+
+        if( numberOfResults == 1) {
+            return true;
+        }
+        
+        else {
+            return false;
+        }     
+ 
     }
 
 }
